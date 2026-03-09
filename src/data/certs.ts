66245a4e14abc596110adb5de33fb418a3f207c6
Ivana Jones <ivanajns@gmail.com>
2026-03-09 16:20:18 -0400

    Move portfolio images to img root and update all code references

--- src/data/certs.ts
+++ src/data/certs.ts
@@ -10,21 +10,21 @@ export const certs: Cert[] = [
   {
     id: 1,
     title: "Coursera: Google Business Intelligence",
-    image: "/img/portfolio/cert-1.jpg",
+    image: "/img/cert-1.jpg",
     url: "https://www.credly.com/badges/df2933f5-516c-4e35-936e-45ecc7862875",
     tags: ["Data Analytics", "Tableau", "Power BI", "SQL"]
   },
   {
     id: 2,
     title: "Python Certified: Associate Data Analyst with Python",
-    image: "/img/portfolio/cert-2.jpg",
+    image: "/img/cert-2.jpg",
     url: "https://www.credly.com/badges/d8a70381-6213-4752-a16a-d93fe141e728",
     tags: ["Data Analysis", "Python", "Pandas", "SQL"]
   },
   {
     id: 3,
     title: "CSSC Certified: Lean Six Sigma Green Belt",
-    image: "/img/portfolio/cert-3.jpg",
+    image: "/img/cert-3.jpg",
     url: "https://badgr.com/public/assertions/B42VPRpaQV6Vz6WuYFB6ug",
     tags: ["Process Improvement", "Change Management"]
   }

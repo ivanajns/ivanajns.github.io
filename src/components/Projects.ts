b51abf781cf41cccf4cb1548a043d291fe3cd4ff
ivanajns <ivanajns@gmail.com>
2026-04-29 14:52:57 -0400

    Update Projects.ts

--- src/components/Projects.ts
+++ src/components/Projects.ts
@@ -10,7 +10,7 @@ export const projects: Project[] = [
     description: "",
     results: "",
     image: project3Src.src,
-    tags: ["dbt", "Snowflake", "Data Modeling", "Data Warehouse", "Transformation Layer", "ELT", "SQL"],
+    tags: ["dbt", "Snowflake", "Data Modeling", "Kimball", "Data Warehouse", "Transformation Layer", "ELT", "SQL"],
     featured: true
   },
   {

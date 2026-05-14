aaaa8561386708766fa4449aa9aab4f6e08ea922
ivanajns <ivanajns@gmail.com>
2026-05-14 16:07:49 -0400

    Update tags for Analytics Engineering project

--- src/components/Projects.ts
+++ src/components/Projects.ts
@@ -10,7 +10,7 @@ export const projects: Project[] = [
     description: "",
     results: "",
     image: project3Src.src,
-    tags: ["dbt", "Snowflake", "Data Modeling", "Kimball", "SQL", "Data Warehouse", "Transformation Layer", "ELT"],
+    tags: ["dbt", "Snowflake Views", "Dimensional Modeling", "Kimball", "SQL", "Data Warehouse", "Transformation Layer", "ELT"],
     featured: true
   },
   {

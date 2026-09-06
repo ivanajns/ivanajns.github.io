3e5460cc0d6d2beb1d0261d42fdd17597bb64008
ivanajns <ivanajns@gmail.com>
2026-09-06 01:42:48 -0400

    Update project title for consistency

--- src/components/Projects.ts
+++ src/components/Projects.ts
@@ -34,7 +34,7 @@ export const projects: Project[] = [
   },
 {
     id: 4,
-    title: "End-to-End Fabric Analytics Engineering Solution in Github",
+    title: "Microsoft Fabric Analytics Engineering Solution in Github",
     description: "",
     results: "",
     image: project3Src.src,

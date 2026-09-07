ef4a5bf3f7b596124ca93db33a49459a410fcb34
ivanajns <ivanajns@gmail.com>
2026-09-07 15:53:09 -0400

    Update tags for project3 in Projects.ts

--- src/components/Projects.ts
+++ src/components/Projects.ts
@@ -38,7 +38,7 @@ export const projects: Project[] = [
     description: "",
     results: "",
     image: project3Src.src,
-    tags: ["Unified Analytics Platform", "Data Quality", "Semantic Modeling", "Dimensional Data Modeling", "Metrics Dashboard"],
+    tags: ["Unified Analytics Platform", "Data Quality", "Semantic Models", "Dimensional Data Models", "Metrics Dashboard"],
     featured: true
   }
 ];
